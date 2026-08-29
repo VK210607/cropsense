434ea8ca42b872dd32588ef772ede9eac339161e
Vijay Kiran <vijaykiran2106@gmail.com>
2026-08-29 22:12:51 +0530

    Initial commit: Spring Boot base project with pom.xml

---
+++ src/main/java/javaproject/cropsense/CropsenseApplication.java
@@ -0,0 +1,13 @@
+package javaproject.cropsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropsenseApplication.class, args);
+	}
+
+}
